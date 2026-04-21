d67c52c15c3f9663eed77d30a8b0178ef152f31c
yuraavagyan <yura.yura.avagyan@gmail.com>
2026-04-21 12:33:31 +0400

    setup folder architecture

--- app/backend/api/endpoints/influencers.py
+++ app/backend/api/endpoints/influencers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Query
 from typing import List, Optional
-from app.schemas.influencer import Influencer, InfluencerCreate, InfluencerUpdate
+from schemas.influencer import Influencer, InfluencerCreate, InfluencerUpdate
 
 router = APIRouter()
 
--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.core.config import settings
-from app.api.api import api_router
+from core.config import settings
+from api.api import api_router
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
